35d003ab3e1df6c3f128949c94027ecfa658cab1
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-01-16 17:21:04 -0300

    feat: new route and fav component

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,6 +1,4 @@
 import styles from './Inicio.module.css';
-import Rodape from "../../components/Rodape";
-import Cabecalho from "../../components/Cabecalho";
 import Banner from "../../components/Banner";
 import Titulo from "../../components/Titulo";
 import Card from "../../components/Card";
@@ -9,7 +7,6 @@ import videos from '../../json/db.json';
 export default function Inicio() {
     return (
         <>
-            <Cabecalho />
             <Banner imagem="home" />
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
@@ -19,7 +16,6 @@ export default function Inicio() {
                     return <Card {...video} key={video.id} />
                 })}
             </section>
-            <Rodape />
         </>
     )
 }
