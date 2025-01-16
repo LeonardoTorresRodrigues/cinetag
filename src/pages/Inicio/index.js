ef7c6dc5dc2003e96d4a1c95e8b62965d419bed3
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-01-16 16:50:58 -0300

    style: add banner

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,12 @@
 import Rodape from "../../components/Rodape";
 import Cabecalho from "../../components/Cabecalho";
+import Banner from "../../components/Banner";
 
 export default function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="home" />
             <Rodape />
         </>
     )
